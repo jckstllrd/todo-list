d7780112d854bd6cf1bb050d80aae7cf81b6dc96
jckstllrd <jackstallard17@gmail.com>
2024-09-27 16:37:47 +0100

    Add TodoItem Module

--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,17 @@
-const todo = () => {};
+const todo = () => {
+  let title;
+  let description;
+  let dueDate;
+  let priority;
+  let notes;
+  let checklist;
+
+  const updateTitle = (title) => {
+    this.title = title;
+  };
+  const updateDescription = (description) => {
+    this.description = description;
+  };
+
+  return { updateTitle, updateDescription };
+};
