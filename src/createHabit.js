598b3e5c0b04c27cce11f87fcb2fbe5b9944fd1e
jckstllrd <jackstallard17@gmail.com>
2024-10-01 18:02:50 +0100

    Refactoring to be a Habit tracking app

---
+++ src/createHabit.js
@@ -0,0 +1,15 @@
+export default habit = () => {
+  let title;
+  let desc;
+  let type;
+  const createHabit = (title, desc, type) => {
+    this.title = title;
+    this.desc = desc;
+    this.type = type;
+  }
+  const display
+
+  return {createHabit}
+}
+
+export {habit}
---
+++ src/domController.js
@@ -0,0 +1,16 @@
+export const domController = () => {
+    /**
+     * Functionality to load anything on the dom
+     * 
+     * Items to load:
+     * 1. Any new lists in the sidebar
+     * 2. The content of lists in the main content section
+     *      - How should these link?
+     * 3. Will this function give functionality to all the initial
+     *    dialog boxes?
+     * 
+     */
+    const displayHabit = (habit) => {
+        
+    }
+}
