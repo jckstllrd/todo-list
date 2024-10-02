81f80bb62bc1247f6b9e068daaa3adb618f0476d
jckstllrd <jackstallard17@gmail.com>
2024-10-02 07:58:25 +0100

    Add function for initialising buttons on the page

--- src/domController.js
+++ src/domController.js
@@ -1,16 +1,30 @@
 export const domController = () => {
-    /**
-     * Functionality to load anything on the dom
-     * 
-     * Items to load:
-     * 1. Any new lists in the sidebar
-     * 2. The content of lists in the main content section
-     *      - How should these link?
-     * 3. Will this function give functionality to all the initial
-     *    dialog boxes?
-     * 
-     */
-    const displayHabit = (habit) => {
-        
-    }
-}
+  /**
+   * Functionality to load anything on the dom
+   *
+   * Items to load:
+   * 1. Any new lists in the sidebar
+   * 2. The content of lists in the main content section
+   *      - How should these link?
+   * 3. Will this function give functionality to all the initial
+   *    dialog boxes?
+   *
+   */
+  const displayHabit = (habit) => {};
+
+  const initialiseEvents = () => {
+    const newHabitBtn = document.querySelector(".new-habit.btn");
+    const newListBtn = document.querySelector(".new-list.btn");
+
+    const newHabitDialog = document.querySelector(".new-habit.dialog");
+    const newListDialog = document.querySelector(".new-list.dialog");
+
+    newHabitBtn.addEventListener("click", () => {
+      newHabitDialog.showModal();
+    });
+    newListBtn.addEventListener("click", () => {
+        newListDialog.showModal();
+      });
+  };
+  return { displayHabit, initialiseEvents}
+};
