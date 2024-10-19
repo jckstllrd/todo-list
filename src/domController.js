bdab51c8383b4f8f56e71803cc2c7491b22d0e6f
jckstllrd <jackstallard17@gmail.com>
2024-10-19 10:19:30 +0100

    Add createHabit and extracting from new habit form

--- src/domController.js
+++ src/domController.js
@@ -1,5 +1,26 @@
+import {createHabit} from "./habit";
+
 const domController = (function () {
   // const displayHabit = (habit) => {};
+
+  const extractHabit = () => {
+    const title = document.querySelector("#title").value;
+    const desc = document.querySelector(
+      'input[name="habit-description"]'
+    ).value;
+    const type = document.querySelector(
+      '#habit-type'
+    ).value;
+    console.log("habit logged");
+    let newHabit = createHabit(title, desc, type);
+    console.log(newHabit);
+    
+    const habitJSON = JSON.stringify(newHabit);
+    console.log(habitJSON);
+
+    localStorage.setItem(title, habitJSON);
+    console.log(localStorage.getItem(title))
+  };
 
   const loadPage = () => {
     console.log("here");
@@ -10,18 +31,30 @@ const domController = (function () {
     const newHabitDialog = document.querySelector(".new-habit.dialog");
     const newListDialog = document.querySelector(".new-list.dialog");
 
+    const newHabitDialogSubmit = document.querySelector(".habit-submit");
+    newHabitDialogSubmit.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("submitting");
+      extractHabit();
+      newHabitDialog.close();
+    });
+
     newHabitBtn.addEventListener("click", () => {
       newHabitDialog.showModal();
-      console.log("opened");
+      console.log("opened 1");
     });
     newListBtn.addEventListener("click", () => {
       newListDialog.showModal();
-      console.log("opened");
+      console.log("opened 2");
+    });
+    const closeMenuBtns = document.querySelectorAll(".close-menu.btn");
+    closeMenuBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        e.target.offsetParent.close();
+      });
     });
   };
   return { loadPage };
 })();
-
-domController.loadPage();
 
 export { domController };
--- src/habit.js
+++ src/habit.js
@@ -1,21 +1,8 @@
-export default habit = () => {
-  let title;
-  let desc;
-  let type;
+function createHabit (newTitle, newDesc, newType){
+  const title = newTitle;
+  const description = newDesc;
+  const type = newType;
 
-  const createHabit = (title, desc, type) => {
-    this.title = title;
-    this.desc = desc;
-    this.type = type;
-  }
+  return {title, description, type}}
 
-  const getInfo = () => {
-    return {title, desc, type}
-  }
-
-  
-
-  return {createHabit, getInfo}
-}
-
-export {habit}
+export {createHabit}
